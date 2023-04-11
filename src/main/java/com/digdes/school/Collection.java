75f0d423915e6b48f1b66b8d981b7728b637ac18
Anna Dmitrieva <glinchikova.anna@gmail.com>
2023-04-11 22:41:35 +0200

    Validation

--- src/main/java/com/digdes/school/Collection.java
+++ src/main/java/com/digdes/school/Collection.java
@@ -12,11 +12,13 @@ public class Collection {
     }
 
     public List<Map<String, Object>> select(String info) throws Exception {
+        validation(info);
         List<Map<String, Object>> select = search(info);
         return select;
     }
 
     public List<Map<String, Object>> insert(String info) throws Exception {
+        validation(info);
         Map<String, Object> row = new HashMap<>();
         row = putMap(info, row);
         List<Map<String, Object>> output = new ArrayList<>();
@@ -33,6 +35,7 @@ public class Collection {
     }
 
     public List<Map<String, Object>> delete(String info) throws Exception {
+        validation(info);
         List<Map<String, Object>> output = new ArrayList<>();
         if (info.isEmpty()) {
             output.addAll(data);
@@ -45,10 +48,10 @@ public class Collection {
         }
         return output;
     }
-
     public List<Map<String, Object>> update(String info) throws Exception {
+        validation(info);
         List<Map<String, Object>> output = new ArrayList<>();
-        List<Map<String, Object>> out = new ArrayList<>();
+        List<Map<String, Object>> out;
         String where = info.replaceFirst("\\s*(?i)values\\s*('(\\s*[A-Z|a-z|А-Я|а-я]+\\s*)+'\\s*=\\s*" +
                 "('(\\s*[A-Z|a-z|А-Я|а-я]+\\s*)+'|\\d+\\.?\\d*|[A-Z|a-z|А-Я|а-я]+)\\s*,\\s*)*" +
                 "('(\\s*[A-Z|a-z|А-Я|а-я]+\\s*)+'\\s*=\\s*('(\\s*[A-Z|a-z|А-Я|а-я]+\\s*)+'|\\d+\\.?\\d*|" +
@@ -78,6 +81,7 @@ public class Collection {
                 }
                 if (map.isEmpty()) {
                     System.out.println("Строка пуста. Строка удалена.");
+                    out.remove(map);
                 } else {
                     data.add(map);
                 }
@@ -91,149 +95,147 @@ public class Collection {
         return output;
     }
 
-    private List<Map<String, Object>> search(String info) throws Exception {
-        List<Map<String, Object>> result = new ArrayList<>();
-        if (info.matches("(?i)where\\s*('(\\s*[A-Z|a-z|А-Я|а-я]+\\s*)+'\\s*(=|!=|(?i)like|(?i)ilike|>=|<=|<|>)" +
-                "\\s*('(%?\\s*%?[A-Z|a-z|А-Я|а-я]*%?\\s*%?)+'|\\d+\\.?\\d*|[A-Z|a-z|А-Я|а-я]+)\\s*((?i)and|(?i)or)\\s*)*" +
+    private void validation(String info) throws Exception {
+        if (!info.matches("((?i)values\\s*('(\\s*[A-Z|a-z|А-Я|а-я]+\\s*)+'\\s*=\\s*('(\\s*[A-Z|a-z|А-Я|а-я]+\\s*)+'" +
+                "|\\d+\\.?\\d*|[A-Z|a-z|А-Я|а-я]+)\\s*,\\s*)*('(\\s*[A-Z|a-z|А-Я|а-я]+\\s*)+'\\s*=\\s*" +
+                "('(\\s*[A-Z|a-z|А-Я|а-я]+\\s*)+'|\\d+\\.?\\d*|[A-Z|a-z|А-Я|а-я]+)\\s*))?" +
+                "((?i)where\\s*(\\(*\\s*'(\\s*[A-Z|a-z|А-Я|а-я]+\\s*)+'\\s*(=|!=|(?i)like|(?i)ilike|>=|<=|<|>)" +
+                "\\s*('(%?\\s*%?[A-Z|a-z|А-Я|а-я]*%?\\s*%?)+'|\\d+\\.?\\d*|[A-Z|a-z|А-Я|а-я]+)\\s*\\)*\\s*((?i)and|(?i)or)\\s*)*" +
                 "('(\\s*[A-Z|a-z|А-Я|а-я]+\\s*)+'\\s*(=|!=|(?i)like|(?i)ilike|>=|<=|<|>)\\s*" +
-                "('(%?\\s*%?[A-Z|a-z|А-Я|а-я]*%?\\s*%?)+'|\\d+\\.?\\d*|[A-Z|a-z|А-Я|а-я]+)\\s*)")) {
-            List<String> matches = new ArrayList<>();
-            Pattern pattern = Pattern.compile("('(\\s*[A-Z|a-z|А-Я|а-я]+\\s*)+'\\s*(=|!=|(?i)like|(?i)ilike|>=|<=|<|>)\\s*" +
-                    "('(%?\\s*%?[A-Z|a-z|А-Я|а-я]*%?\\s*%?)+'|\\d+\\.?\\d*|[A-Z|a-z|А-Я|а-я]+))|(?i)AND|(?i)OR");
-            Matcher matcher = pattern.matcher(info);
-            while (matcher.find()) {
-                matches.add(matcher.group());
-            }
-            List<Map<String, Object>> all = new ArrayList<>();
-            List<Map<String, Object>> temp = new ArrayList<>();
-            for (String match : matches) {
-                if (!match.equalsIgnoreCase("and") && !match.equalsIgnoreCase("or")) {
-                    all = compare(match, all);
-                }
-            }
-            if (!info.matches(".+(?i)and.+")) {
-                result = all;
-            } else {
-                temp.addAll(all);
-                for (int i = 0; i < matches.size(); i++) {
-                    if (matches.get(i).equalsIgnoreCase("and")) {
-                        temp = and(matches.get(i - 1), temp);
-                        temp = and(matches.get(i + 1), temp);
-                        if (i == matches.size() - 2) {
-                            result.addAll(temp);
-                        } else if (matches.get(i + 2).equalsIgnoreCase("or")) {
-                            result.addAll(temp);
-                            temp.clear();
-                            temp.addAll(all);
-                        }
-                    }
-                    if (matches.get(i).equalsIgnoreCase("or")) {
-                        if (i == matches.size() - 2) {
-                            compare(matches.get(i + 1), result);
-                        } else if (i == 1 && matches.get(i + 2).equalsIgnoreCase("and")) {
-                            compare(matches.get(i - 1), result);
-                        } else if (i == 1 && matches.get(i + 2).equalsIgnoreCase("or")) {
-                            compare(matches.get(i - 1), result);
-                            compare(matches.get(i + 1), result);
-                        } else if (matches.get(i + 2).equalsIgnoreCase("or")) {
-                            compare(matches.get(i + 1), result);
-                        }
-                    }
-                }
-            }
-        } else {
+                "('(%?\\s*%?[A-Z|a-z|А-Я|а-я]*%?\\s*%?)+'|\\d+\\.?\\d*|[A-Z|a-z|А-Я|а-я]+)\\s*\\)*\\s*))?")) {
             Exception ex = new Exception("Некорректный запрос");
             throw ex;
+        }
+    }
+
+    private List<Map<String, Object>> search(String info) throws Exception {
+        List<Map<String, Object>> result = new ArrayList<>();
+        List<String> matches = new ArrayList<>();
+        Pattern pattern = Pattern.compile("('(\\s*[A-Z|a-z|А-Я|а-я]+\\s*)+'\\s*(=|!=|(?i)like|(?i)ilike|>=|<=|<|>)\\s*" +
+                "('(%?\\s*%?[A-Z|a-z|А-Я|а-я]*%?\\s*%?)+'|\\d+\\.?\\d*|[A-Z|a-z|А-Я|а-я]+))|(?i)AND|(?i)OR");
+        Matcher matcher = pattern.matcher(info);
+        while (matcher.find()) {
+            matches.add(matcher.group());
+        }
+        List<Map<String, Object>> all = new ArrayList<>();
+        List<Map<String, Object>> temp = new ArrayList<>();
+        for (String match : matches) {
+            if (!match.equalsIgnoreCase("and") && !match.equalsIgnoreCase("or")) {
+                all = compare(match, all);
+            }
+        }
+        if (!info.matches(".+(?i)and.+")) {
+            result = all;
+        } else {
+            temp.addAll(all);
+            for (int i = 0; i < matches.size(); i++) {
+                if (matches.get(i).equalsIgnoreCase("and")) {
+                    temp = and(matches.get(i - 1), temp);
+                    temp = and(matches.get(i + 1), temp);
+                    if (i == matches.size() - 2) {
+                        result.addAll(temp);
+                    } else if (matches.get(i + 2).equalsIgnoreCase("or")) {
+                        result.addAll(temp);
+                        temp.clear();
+                        temp.addAll(all);
+                    }
+                }
+                if (matches.get(i).equalsIgnoreCase("or")) {
+                    if (i == matches.size() - 2) {
+                        compare(matches.get(i + 1), result);
+                    } else if (i == 1 && matches.get(i + 2).equalsIgnoreCase("and")) {
+                        compare(matches.get(i - 1), result);
+                    } else if (i == 1 && matches.get(i + 2).equalsIgnoreCase("or")) {
+                        compare(matches.get(i - 1), result);
+                        compare(matches.get(i + 1), result);
+                    } else if (matches.get(i + 2).equalsIgnoreCase("or")) {
+                        compare(matches.get(i + 1), result);
+                    }
+                }
+            }
         }
         return result;
     }
 
     private Map<String, Object> putMap(String info, Map<String, Object> row) throws Exception {
-        if (info.matches("(?i)values\\s*('(\\s*[A-Z|a-z|А-Я|а-я]+\\s*)+'\\s*=\\s*('(\\s*[A-Z|a-z|А-Я|а-я]+\\s*)+'" +
-                "|\\d+\\.?\\d*|[A-Z|a-z|А-Я|а-я]+)\\s*,\\s*)*('(\\s*[A-Z|a-z|А-Я|а-я]+\\s*)+'\\s*=\\s*" +
-                "('(\\s*[A-Z|a-z|А-Я|а-я]+\\s*)+'|\\d+\\.?\\d*|[A-Z|a-z|А-Я|а-я]+)\\s*)")) {
-            List<String> matches = new ArrayList<>();
-            Pattern pattern = Pattern.compile("'(\\s*[A-Z|a-z|А-Я|а-я]+\\s*)+'\\s*=\\s*" +
-                    "('(\\s*[A-Z|a-z|А-Я|а-я]+\\s*)+'|\\d+\\.?\\d*|[A-Z|a-z|А-Я|а-я]+)");
-            Matcher matcher = pattern.matcher(info);
-            while (matcher.find()) {
-                matches.add(matcher.group());
-            }
-            for (String match : matches) {
-                if (!match.matches("(?i)'id'.+|(?i)'lastName'.+|(?i)'age'.+|(?i)'cost'.+|(?i)'active'.+")) {
-                    Exception ex = new Exception("Некорректное название колонки");
-                    throw ex;
-                }
-                if (match.matches("(?i)'id'.+")) {
-                    String number = match.replaceFirst("'.+'\\s*=\\s*", "");
-                    if (number.equals("null")) {
-                        row.remove("id");
-                    } else {
-                        try {
-                            long id = Long.parseLong(number);
-                            row.put("id", id);
-                        } catch (Exception ex) {
-                            System.out.println("Некорректный тип данных для поля id");
-                        }
-                    }
-                }
-                if (match.matches("(?i)'lastName'.+")) {
-                    String str = match.replaceFirst("'.+'\\s*=\\s*", "");
-                    if (str.equals("null")) {
-                        row.remove("lastName");
-                    }
-                    if (!str.equals("null") && !str.matches("'(\\s*[A-Z|a-z|А-Я|а-я]+\\s*)+'")) {
-                        System.out.println("Некорректный тип данных для поля lastName");
-                    }
-                    if (str.matches("'(\\s*[A-Z|a-z|А-Я|а-я]+\\s*)+'")) {
-                        String lastName = str.replaceAll("'", "");
-                        row.put("lastName", lastName);
-                    }
-                }
-                if (match.matches("(?i)'age'.+")) {
-                    String number = match.replaceFirst("'.+'\\s*=\\s*", "");
-                    if (number.equals("null")) {
-                        row.remove("age");
-                    } else {
-                        try {
-                            long age = Long.parseLong(number);
-                            row.put("age", age);
-                        } catch (Exception ex) {
-                            System.out.println("Некорректный тип данных для поля age");
-                        }
-                    }
-                }
-                if (match.matches("(?i)'cost'.+")) {
-                    String number = match.replaceFirst("'.+'\\s*=\\s*", "");
-                    if (number.equals("null")) {
-                        row.remove("cost");
-                    } else {
-                        try {
-                            double cost = Double.parseDouble(number);
-                            row.put("cost", cost);
-                        } catch (Exception ex) {
-                            System.out.println("Некорректный тип данных для поля cost");
-                        }
-                    }
-                }
-                if (match.matches("(?i)'active'.+")) {
-                    String bool = match.replaceFirst("'.+'\\s*=\\s*", "");
-                    if (bool.equals("true")) {
-                        row.put("active", true);
-                    } else if (bool.equals("false")) {
-                        row.put("active", false);
-                    } else if (bool.equals("null")) {
-                        row.remove("active");
-                    } else {
-                        System.out.println("Некорректный тип данных для поля active");
-                    }
-                }
-            }
-            return row;
-        } else {
-            Exception ex = new Exception("Некорректный запрос");
-            throw ex;
+        List<String> matches = new ArrayList<>();
+        Pattern pattern = Pattern.compile("'(\\s*[A-Z|a-z|А-Я|а-я]+\\s*)+'\\s*=\\s*" +
+                "('(\\s*[A-Z|a-z|А-Я|а-я]+\\s*)+'|\\d+\\.?\\d*|[A-Z|a-z|А-Я|а-я]+)");
+        Matcher matcher = pattern.matcher(info);
+        while (matcher.find()) {
+            matches.add(matcher.group());
         }
+        for (String match : matches) {
+            if (!match.matches("(?i)'id'.+|(?i)'lastName'.+|(?i)'age'.+|(?i)'cost'.+|(?i)'active'.+")) {
+                Exception ex = new Exception("Некорректное название колонки");
+                throw ex;
+            }
+            if (match.matches("(?i)'id'.+")) {
+                String number = match.replaceFirst("'.+'\\s*=\\s*", "");
+                if (number.equals("null")) {
+                    row.remove("id");
+                } else {
+                    try {
+                        long id = Long.parseLong(number);
+                        row.put("id", id);
+                    } catch (Exception ex) {
+                        System.out.println("Некорректный тип данных для поля id");
+                    }
+                }
+            }
+            if (match.matches("(?i)'lastName'.+")) {
+                String str = match.replaceFirst("'.+'\\s*=\\s*", "");
+                if (str.equals("null")) {
+                    row.remove("lastName");
+                }
+                if (!str.equals("null") && !str.matches("'(\\s*[A-Z|a-z|А-Я|а-я]+\\s*)+'")) {
+                    System.out.println("Некорректный тип данных для поля lastName");
+                }
+                if (str.matches("'(\\s*[A-Z|a-z|А-Я|а-я]+\\s*)+'")) {
+                    String lastName = str.replaceAll("'", "");
+                    row.put("lastName", lastName);
+                }
+            }
+            if (match.matches("(?i)'age'.+")) {
+                String number = match.replaceFirst("'.+'\\s*=\\s*", "");
+                if (number.equals("null")) {
+                    row.remove("age");
+                } else {
+                    try {
+                        long age = Long.parseLong(number);
+                        row.put("age", age);
+                    } catch (Exception ex) {
+                        System.out.println("Некорректный тип данных для поля age");
+                    }
+                }
+            }
+            if (match.matches("(?i)'cost'.+")) {
+                String number = match.replaceFirst("'.+'\\s*=\\s*", "");
+                if (number.equals("null")) {
+                    row.remove("cost");
+                } else {
+                    try {
+                        double cost = Double.parseDouble(number);
+                        row.put("cost", cost);
+                    } catch (Exception ex) {
+                        System.out.println("Некорректный тип данных для поля cost");
+                    }
+                }
+            }
+            if (match.matches("(?i)'active'.+")) {
+                String bool = match.replaceFirst("'.+'\\s*=\\s*", "");
+                if (bool.equals("true")) {
+                    row.put("active", true);
+                } else if (bool.equals("false")) {
+                    row.put("active", false);
+                } else if (bool.equals("null")) {
+                    row.remove("active");
+                } else {
+                    System.out.println("Некорректный тип данных для поля active");
+                }
+            }
+        }
+        return row;
     }
 
     private List<Map<String, Object>> compare(String match, List<Map<String, Object>> all) throws Exception {
