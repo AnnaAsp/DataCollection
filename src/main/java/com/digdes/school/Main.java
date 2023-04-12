9aa4ddca9d09909e36b8a94faa48e15e51f0e783
Anna Dmitrieva <glinchikova.anna@gmail.com>
2023-04-12 20:14:12 +0200

    Data Collection

--- src/main/java/com/digdes/school/Main.java
+++ src/main/java/com/digdes/school/Main.java
@@ -1,16 +1,15 @@
 package com.digdes.school;
 
 import java.util.Scanner;
-import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        JavaSchoolStarter collection = new JavaSchoolStarter();
+        JavaSchoolStarter starter = new JavaSchoolStarter();
 
         while (true) {
             System.out.println("Пожалуйста, введите запрос");
             String in =  new Scanner(System.in).nextLine();
-            collection.execute(in);
+            starter.execute(in);
         }
 
     }
